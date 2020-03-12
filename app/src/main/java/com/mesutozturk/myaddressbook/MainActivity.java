f2b24f9dc360539d2969686d1d1053ae57358b7f
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2020-03-12 14:22:48 +0300

    add person activity

--- app/src/main/java/com/mesutozturk/myaddressbook/MainActivity.java
+++ app/src/main/java/com/mesutozturk/myaddressbook/MainActivity.java
@@ -2,13 +2,25 @@ package com.mesutozturk.myaddressbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNew = findViewById(R.id.btn_new);
+
+        btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AddPersonActivity.class));
+            }
+        });
     }
 }
